bf894c23abe5dff7326c9418dc7e11a33fe1af49
chief <zhaoyuchief@163.com>
2017-05-18 22:02:04 +0800

    Create package.js

---
+++ package.js
@@ -0,0 +1,25 @@
+{
+  "name": "helloword",
+  "version": "1.0.0",
+  "description": "helloword",
+  "main": "index.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "repository": {
+    "type": "git",
+    "url": "git+https://github.com/zhaoyuchief/helloword.git"
+  },
+  "keywords": [
+    "express"
+  ],
+  "author": "chief",
+  "license": "MIT",
+  "bugs": {
+    "url": "https://github.com/zhaoyuchief/helloword/issues"
+  },
+  "homepage": "https://github.com/zhaoyuchief/helloword#readme",
+  "dependencies": {
+    "express": "^4.15.3"
+  }
+}
